e933a236a004ae7363f776a3534a61ef3b127ddd
p7m <perpetuum7@protonmail.com>
2022-08-29 09:36:55 +0200

    fix: adjust rollup config to ignore build errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import image from '@rollup/plugin-image';
+import resolve from '@rollup/plugin-node-resolve';
+import sucrase from '@rollup/plugin-sucrase';
 import svgr from '@svgr/rollup';
 import css from 'rollup-plugin-import-css';
-import image from '@rollup/plugin-image';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
 
 const packageJson = require('./package.json');
 
@@ -12,12 +13,25 @@ export default {
 	plugins: [
 		peerDepsExternal(),
 		resolve(),
-		commonjs(),
+		commonjs({
+			include: 'node_modules/**',
+			namedExports: {
+				'node_modules/react-is/index.js': ['isMemo']
+			}
+		}),
 		typescript({ tsconfig: 'tsconfig.rollup.json', useTsconfigDeclarationDir: true }),
+		sucrase({
+			exclude: ['node_modules/**'],
+			transforms: ['typescript', 'jsx']
+		}),
 		css(),
 		image(),
 		svgr()
 	],
+	moduleContext: {
+		'./node_modules/react-icons/lib/esm/iconBase.js': 'window'
+	},
+    external: ['rc-util', 'rc-util/es/hooks/useState'],
 	input: 'src/index.ts',
 	output: [
 		{
